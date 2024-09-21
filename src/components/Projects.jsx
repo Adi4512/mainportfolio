4f647c9281e778148edee7a554b69b85d0bba408
Adi4512 <adityasharma4510@gmail.com>
2024-09-22 02:42:07 +0530

    favicon changed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,16 +30,24 @@ export default function Projects() {
                     alt={p.title}
                      />
                 </motion.div>
-            <div className='w-full max-w-xl lg:w-3/4'>
+            <motion.div
+            whileInView={{opacity:1,x:0}}
+            initial={{opacity:0,x:100}}
+            transition={{duration:1}}
+            
+            
+            
+             className='w-full max-w-xl lg:w-3/4'>
             <h6 className='mb-2 font-semibold'>{p.title}</h6>
             <p className='mb-4 text-neutral-400'>{p.description}</p>
             {p.technologies.map((tech,index)=>{
                 return (
                     <span key={index}
-                    className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>{tech}</span>
+                    className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>
+                    {tech}</span>
                 )
             })}
-            </div>
+            </motion.div>
             </div>
 
             )
